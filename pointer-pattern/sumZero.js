244de1da3152b0673f8e839121296c979022868c
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-21 14:55:12 -0700

    do some small refactor

---
+++ pointer-pattern/sumZero.js
@@ -0,0 +1,25 @@
+function sumZero(arr) {
+  let i = 0;
+  let j = arr.length - 1;
+  while (i < j) {
+    let sum = arr[i] + arr[j];
+    if (sum === 0) {
+      return [arr[i] + arr[j]];
+    } else if (sum > 0) {
+      j--;
+    } else if (sum < 0) {
+      i++;
+    }
+  }
+}
+
+// naive
+// function sumZero(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i] + arr[j]];
+//       }
+//     }
+//   }
+// }
