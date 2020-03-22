dd2ccde6f55d7a5fe4132cdbbd5b3286aef8d930
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-22 01:30:09 -0700

    refactor countUniqueValues

--- pointer-pattern/countUniqueValues.js
+++ pointer-pattern/countUniqueValues.js
@@ -1,16 +1,12 @@
 function countUniqueValues(arr) {
-  let i = 0;
-  let j = 1;
+  var i = 0;
   if (!arr.length) {
     return 0;
   }
-  while (j < arr.length) {
-    if (arr[i] === arr[j]) {
-      j++;
-    } else {
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
       i++;
       arr[i] = arr[j];
-      j++;
     }
   }
   return i + 1;
