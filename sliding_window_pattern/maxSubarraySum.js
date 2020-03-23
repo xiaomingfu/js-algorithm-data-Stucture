bc356275c9b03783b3a38c929320b86d6e363b73
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-22 17:44:31 -0700

    sliding window pattern solution

--- sliding_window_pattern/maxSubarraySum.js
+++ sliding_window_pattern/maxSubarraySum.js
@@ -1,16 +1,32 @@
+// function maxSubarraySum(arr, int) {
+//   if (n > arr.length) {
+//     return null;
+//   }
+//   let sum = -Infinity;
+//   for (let i = 0; i < arr.length - int + 1; i++) {
+//     let temp = 0;
+//     for (let j = 0; j < int; j++) {
+//       temp += arr[i + j];
+//     }
+//     if (temp > sum) {
+//       sum = temp;
+//     }
+//   }
+//   return sum;
+// }
+
+// O(n)
 function maxSubarraySum(arr, int) {
-  if (n > arr.length) {
-    return null;
+  if (arr.length < int) return null;
+  let maxSum = 0;
+  let tempSum = 0;
+  for (let i = 0; i < int; i++) {
+    maxSum += arr[i];
   }
-  let sum = -Infinity;
-  for (let i = 0; i < arr.length - int + 1; i++) {
-    let temp = 0;
-    for (let j = 0; j < int; j++) {
-      temp += arr[i + j];
-    }
-    if (temp > sum) {
-      sum = temp;
-    }
+  tempSum = maxSum;
+  for (let i = int; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - int] + arr[i];
+    maxSum = math.max(maxSum, tempSum);
   }
-  return sum;
+  return maxSum;
 }
