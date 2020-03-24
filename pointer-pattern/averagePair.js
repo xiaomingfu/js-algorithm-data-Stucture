5f7705091f44e86b0543e53104d8669472683fe6
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-23 17:07:40 -0700

    solve averagePair by multiple pointer

---
+++ pointer-pattern/averagePair.js
@@ -0,0 +1,20 @@
+// give a sorted arr of int and a number, if there is a pair of values where the average equals the number return true, else return false
+
+function averagePair(arr, num) {
+  if (arr.length < 2) {
+    return false;
+  }
+  let small = 0;
+  let big = arr.length - 1;
+  while (small < big) {
+    let average = (arr[small] + arr[big]) / 2;
+    if (average === num) {
+      return true;
+    } else if (average > num) {
+      big--;
+    } else {
+      small++;
+    }
+  }
+  return false;
+}
