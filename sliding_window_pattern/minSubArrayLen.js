56c0413cf597f18676b52ea75c9a26c1047031b6
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-26 02:28:06 -0700

    refactor minSubArrayLen

--- sliding_window_pattern/minSubArrayLen.js
+++ sliding_window_pattern/minSubArrayLen.js
@@ -1,20 +1,41 @@
 // give an array of positve int and a positive int, if the sum of a contiguous subarray is greater or equal to the int, return the minimal length of  the subarray, else return 0
-function minSubArrayLen(arr, int) {
-  let slice = 1;
-  while (slice <= arr.length) {
-    let sum = 0;
-    for (var i = 0; i < slice; i++) {
-      sum += arr[i];
+// function minSubArrayLen(arr, int) {
+//   let slice = 1;
+//   while (slice <= arr.length) {
+//     let sum = 0;
+//     for (var i = 0; i < slice; i++) {
+//       sum += arr[i];
+//     }
+//     //    iterate arr, compare each element to int
+//     while (i < arr.length + 1) {
+//       if (sum >= int) {
+//         return slice;
+//       }
+//       sum = sum - arr[i - slice] + arr[i];
+//       i++;
+//     }
+//     slice++;
+//   }
+//   return 0;
+// }
+
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+
+  while (start < nums.length) {
+    if (total < sum && end < nums.length) {
+      total += nums[end];
+      end++;
+    } else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= num[start];
+      start++;
+    } else {
+      break;
     }
-    //    iterate arr, compare each element to int
-    while (i < arr.length + 1) {
-      if (sum >= int) {
-        return slice;
-      }
-      sum = sum - arr[i - slice] + arr[i];
-      i++;
-    }
-    slice++;
   }
-  return 0;
+  return minLen === Infinity ? 0 : minLen;
 }
