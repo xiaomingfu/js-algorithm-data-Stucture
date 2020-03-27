f90bbfa32c4650d4e6050dbf508a2f76adf16ce6
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-27 02:09:37 -0700

    add helper_recursion and pure_recursion method

---
+++ recursion/helper_recursion.js
@@ -0,0 +1,9 @@
+function outer(input) {
+  var outerScopedVariable = [];
+  function helper(helperInput) {
+    // modify the outerScopedVariable
+    helper(helperInput--);
+  }
+  helper(input);
+  return outerScopedVariable;
+}
