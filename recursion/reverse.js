768d4149d84c035d6cf6fad4626f9971f90f9e5f
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-28 13:43:15 -0700

    solve reverse

---
+++ recursion/reverse.js
@@ -0,0 +1,7 @@
+// give a string and return a new reversed string
+function reverse(str) {
+  let len = str.length;
+  if (!len) return "";
+
+  return "" + str[len - 1] + reverse(str.slice(0, len - 1));
+}
