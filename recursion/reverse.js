e540043329356ee511e8558ff0f3f0dbc6501dc6
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-29 14:58:27 -0700

    refactor reverse

--- recursion/reverse.js
+++ recursion/reverse.js
@@ -1,7 +1,13 @@
 // give a string and return a new reversed string
-function reverse(str) {
-  let len = str.length;
-  if (!len) return "";
+// function reverse(str) {
+//   let len = str.length;
+//   if (!len) return "";
 
-  return "" + str[len - 1] + reverse(str.slice(0, len - 1));
+//   return "" + str[len - 1] + reverse(str.slice(0, len - 1));
+// }
+function reverse(str) {
+  if (str.length <= 0) {
+    return str;
+  }
+  return reverse(str.slice(1) + str[0]);
 }
