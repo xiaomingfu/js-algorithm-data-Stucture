3d56090ac712fd9aee1b2ba4b79ee7280cc219c5
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-30 01:37:43 -0700

    refactor isPalindrome

--- recursion/isPalindrome.js
+++ recursion/isPalindrome.js
@@ -6,3 +6,10 @@ function isPalindrome(str) {
     return isPalindrome(str.slice(1, len - 1));
   } else return false;
 }
+
+// function isPalindrome(str){
+//  if(str.length === 1) return true;
+// if (str.length === 2) return str[0] === str[1];
+// if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+// return false;
+// }
