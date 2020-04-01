b5cb9e8890a759bc1c4a2ca1d56b7cab13539fd0
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-01 15:22:28 -0700

    solve nestedEvenSum

--- recursion/nestedEvenSum.js
+++ recursion/nestedEvenSum.js
@@ -1,17 +1,14 @@
 // gvie an object, return the sum of all even numbers
 function nestedEvenSum(obj) {
-  let sum = 0;
+  var sum = 0;
 
-  let keys = Object.keys(obj);
-  if (keys.length === 0) {
-    return sum;
-  }
-  for (key of keys) {
-    if (obj.key % 2 === 0) {
-      sum = sum + obj.key;
-    }
-    if (typeof obj.key === "object") {
-      sum = sum + nestedEvenSum(obj.key);
+  let values = Object.values(obj);
+  for (value of values) {
+    if (value % 2 === 0) {
+      sum = sum + value;
+    } else if (typeof value === "object") {
+      sum = sum + nestedEvenSum(value);
     }
   }
+  return sum;
 }
