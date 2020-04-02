060348478868eb14bdc49660707e6e2598741ac9
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-02 01:17:33 -0700

    refactor a bit

--- recursion/nestedEvenSum.js
+++ recursion/nestedEvenSum.js
@@ -4,7 +4,7 @@ function nestedEvenSum(obj) {
 
   let values = Object.values(obj);
   for (let value of values) {
-    if (value % 2 === 0) {
+    if (typeof value === "number" && value % 2 === 0) {
       sum = sum + value;
     } else if (typeof value === "object") {
       sum = sum + nestedEvenSum(value);
