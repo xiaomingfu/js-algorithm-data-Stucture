ef2915f29aa0a040c95f48f6a06b65fa01f89d4b
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-03 00:58:04 -0700

    refactor naiveStringSearch

--- search/naiveStringSearch.js
+++ search/naiveStringSearch.js
@@ -1,20 +1,13 @@
 // give two strings, check if the second string is in the first string, if so count the frequence and return the count
 function naiveStringSearch(str1, str2) {
-  let i = 0;
-
-  let count = 0;
-  while (i < str1.length) {
-    let j = 0;
-    while (j < str2.length) {
-      if (str1[i] === str2[j]) {
-        j++;
-        i++;
-      } else {
-        i++;
+  var count = 0;
+  for (var i = 0; i < str1.length; i++) {
+    for (var j = 0; j < str2.length; j++) {
+      if (str1[i + j] !== str2[j]) {
         break;
       }
     }
-    if (j === str2.length) {
+    if (j === str2.length - 1) {
       count++;
     }
   }
