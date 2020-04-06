14d703db6f8ce8c44bb48c21aee03afb5656be0a
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-05 23:00:14 -0700

    add check if it need swap to bubbleSort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,12 +1,18 @@
 function bubbleSort(arr) {
   // loop through arr with index i from arr.length -1 to start
+  var noSwaps;
   for (let i = arr.length - 1; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j <= i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
+    }
+    if (noSwaps) {
+      break;
     }
   }
   return arr;
