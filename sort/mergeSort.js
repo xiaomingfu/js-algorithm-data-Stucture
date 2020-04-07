c38d6bae12bf49acd7256558280f73fd15d36135
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-06 23:48:08 -0700

    merge code

---
+++ sort/mergeSort.js
@@ -0,0 +1,23 @@
+function merge(arr1, arr2) {
+  let results = [];
+  let j = 0;
+  let i = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
