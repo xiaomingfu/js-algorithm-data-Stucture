815ed9990d37bd229dffd1ff7033af0d2c23597a
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-08 00:00:58 -0700

    add mergeSort

--- sort/merge.js
+++ sort/merge.js
@@ -21,3 +21,13 @@ function merge(arr1, arr2) {
   }
   return results;
 }
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
