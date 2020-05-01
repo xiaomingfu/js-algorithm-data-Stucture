d501dd50774b8536a7a8ccaed454f46ced9f6f41
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-30 17:34:29 -0700

    add push method

---
+++ linkedlist/singlyLinkedList.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(data) {
+    var newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+}
