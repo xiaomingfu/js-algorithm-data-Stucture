9cb02e0c3396db9ca88621043ce788e4ce6a1b0c
Xiaoming Fu <xmingfu86@gmail.com>
2020-05-02 17:12:47 -0700

    add unshift method

--- linkedlist/singlyLinkedList.js
+++ linkedlist/singlyLinkedList.js
@@ -57,4 +57,17 @@ class SinglyLinkedList {
     }
     return temp;
   }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
