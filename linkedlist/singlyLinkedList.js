b7cb361555ddc4a4763f36365b9b6d7fb69fce2b
Xiaoming Fu <xmingfu86@gmail.com>
2020-05-04 15:57:03 -0700

    add reverse method

--- linkedlist/singlyLinkedList.js
+++ linkedlist/singlyLinkedList.js
@@ -131,5 +131,18 @@ class SinglyLinkedList {
     return removed;
   }
 
-  reverse() {}
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
