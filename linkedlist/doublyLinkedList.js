af635e9d45f7bd1b17d3f27a45fc92af2dcba747
Xiaoming Fu <xmingfu86@gmail.com>
2020-05-05 17:22:04 -0700

    add shift and unshift method

--- linkedlist/doublyLinkedList.js
+++ linkedlist/doublyLinkedList.js
@@ -43,4 +43,36 @@ class doublyLinkedList {
     this.length--;
     return temp;
   }
+
+  shift() {
+    if (this.length === 0) {
+      return;
+    }
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let temp = this.head;
+      this.head = temp.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
