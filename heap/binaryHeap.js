39c4239248ca47bd698ef006b39f74a2d05481a3
Xiaoming Fu <xmingfu86@gmail.com>
2020-05-16 09:59:48 -0700

    add insert method to maxBinaryHeap

--- heap/binaryHeap.js
+++ heap/binaryHeap.js
@@ -4,9 +4,24 @@ class MaxBinaryHeap {
   }
 
   insert(value) {
-    let idx = this.values.length - 1;
     this.values.push(value);
+    this.bubbleUp(value);
+    return this;
   }
 
-  bubbleUp(value) {}
+  bubbleUp(value) {
+    let idx = this.values.length - 1;
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (parent >= value) {
+        break;
+      }
+      this.values[parentIdx] = value;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
 }
+
+let heap = new MaxBinaryHeap();
