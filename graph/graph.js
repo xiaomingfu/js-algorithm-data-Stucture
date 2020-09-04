01a623ca60a1577f67fce30e6c603fcdce32e8d3
Xiaoming Fu <xmingfu86@gmail.com>
2020-09-04 00:18:53 -0700

    add breath first search method

--- graph/graph.js
+++ graph/graph.js
@@ -56,6 +56,26 @@ class Graph {
       }
       return result;
   }
+  breadthFirst(start) {
+      const result = [];
+      const visited = {};
+      const currentVertex;
+      const queue = [start];
+      visited[start] = true;
+      
+      while(queue.length) {
+          currentVertex = queue.shift();
+          result.push(currentVertex);
+
+          this.adjacencyList[currentVertex].forEach(neighbor => {
+              if(!visited[neighbor]) {
+                  visited[neighbor] = true;
+                  queue.push(neighbor);
+              }
+          })
+      }
+      return result;
+  }
 }
 
 let g = new Graph();
